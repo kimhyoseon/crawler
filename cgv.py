fca5ed1cec7993264caee8c7d699f5e6cf14e169
kimhyoseon <khs7515@naver>
2018-01-15 17:28:42 +0900

    commit

--- cgv.py
+++ cgv.py
@@ -7,6 +7,10 @@ import filewriter
 import telegrambot
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class Cgv:
 
@@ -20,6 +24,9 @@ class Cgv:
     # 핫딜 카운트
     count = 0
 
+    # 최대 접속시간
+    SITE_CONNECT_TIMEOUT = 5
+
     def __init__(self, args):
         # 기존 로그 가져오기
         self.log = filewriter.get_log_file(self.FILE_NAME)
@@ -30,46 +37,49 @@ class Cgv:
             self.driver = webdriver.PhantomJS('./driver/phantomjs')
 
     def start(self):
-        # Connect to site.
-        self.driver.get(self.SITE_URL)
+        try:
+            # Connect to site.
+            self.driver.get(self.SITE_URL)
+            # product_slist p10 요소를 찾을 때까지 대기
+            element_present = EC.presence_of_element_located((By.XPATH, '//div[@class="evt-item-lst"]'))
+            WebDriverWait(self.driver, self.SITE_CONNECT_TIMEOUT).until(element_present)
+            html = self.driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
 
-        # DOM parse
-        html = self.driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
+            # 1+1 영화 리스트
+            for list in soup.find('div', class_="evt-item-lst").children:
+                print(list)
+                if not list == -1:
+                    linkObj = list.find('a')
+                    imgObj = list.find('img')
+                    if not linkObj == -1:
+                        title = imgObj['alt']
 
-        # 1+1 영화 리스트
-        for list in soup.find('div', class_='evt-item-lst').children:
-            if not list == -1:
-                linkObj = list.find('a')
-                imgObj = list.find('img')
-                if not linkObj == -1:
-                    title = imgObj['alt']
+                        if "1+1" not in ti2tle:
+                            continue
 
-                    if "1+1" not in title:
-                        continue
+                        link = linkObj['href']
+                        img = imgObj['src']
+                        m = re.search('idx=(.*?)\&', link)
+                        id = m.group(1)
+                        link = self.DETAIL_URL + link.replace('./', '')
 
-                    link = linkObj['href']
-                    img = imgObj['src']
-                    m = re.search('idx=(.*?)\&', link)
-                    id = m.group(1)
-                    link = self.DETAIL_URL + link.replace('./', '')
+                        if id and id not in self.log:
+                            text = title + '\n' + link
+                            telegrambot.send_message(text)
+                            self.log.append(id)
+                            self.count = self.count + 1
 
-                    if id and id not in self.log:
-                        text = title + '\n' + link
-                        telegrambot.send_message(text)
-                        self.log.append(id)
-                        self.count = self.count + 1
-                        # print(link)
-                        # print(title)
-                        # print(img)
-                        # print(id)
+            # 결과
+            if self.count > 0:
+                filewriter.save_log_file(self.FILE_NAME, self.log)
+                print('%s: 새로운 핫딜 %d개가 등록 되었습니다.'%(self.FILE_NAME,self.count))
+            else:
+                print('%s: 새로운 핫딜이 없습니다.'%self.FILE_NAME)
 
-        # 결과
-        if self.count > 0:
-            filewriter.save_log_file(self.FILE_NAME, self.log)
-            print('%s: 새로운 핫딜 %d개가 등록 되었습니다.'%(self.FILE_NAME,self.count))
-        else:
-            print('%s: 새로운 핫딜이 없습니다.'%self.FILE_NAME)
+        except Exception as errorMessage:
+            print(errorMessage)
+            telegrambot.send_message(errorMessage)
 
 if __name__ == "__main__":
     # argparse를 사용하여 파라미터 정의
--- lottecinema.py
+++ lottecinema.py
@@ -6,59 +6,72 @@ import filewriter
 import telegrambot
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class Lottecinema:
 
     # 사이트 주소
     SITE_URL = 'http://www.lottecinema.co.kr/LCHS/Contents/Cinema-Mall/gift-shop.aspx'
+
     # 파일명
     FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
 
     # 핫딜 카운트
     count = 0
 
+    # 최대 접속시간
+    SITE_CONNECT_TIMEOUT = 5
+
     def __init__(self, args):
         # 기존 로그 가져오기
         self.log = filewriter.get_log_file(self.FILE_NAME)
 
         if args.chrome is True:
+            # 롯데시네마는 해외IP가 막혀있기 때문에 국내 proxy서버를 설정해준다
             options = webdriver.ChromeOptions()
             options.add_argument('--proxy-server=119.196.234.74:3128')
             self.driver = webdriver.Chrome(executable_path='./driver/chromedriver', chrome_options=options)
-            # self.driver = webdriver.Chrome('./driver/chromedriver')
         else:
-           self.driver = webdriver.PhantomJS(executable_path='./driver/phantomjs', service_args=['--proxy=119.196.234.74:3128'])
+            self.driver = webdriver.PhantomJS(executable_path='./driver/phantomjs', service_args=['--proxy=119.196.234.74:3128'])
 
     def start(self):
-        # Connect to site.
-        self.driver.get(self.SITE_URL)
+        try:
+            # Connect to site.
+            self.driver.get(self.SITE_URL)
+            # product_slist p10 요소를 찾을 때까지 대기
+            element_present = EC.presence_of_element_located((By.XPATH, '//ul[@class="product_slist p10"]'))
+            element = WebDriverWait(self.driver, self.SITE_CONNECT_TIMEOUT).until(element_present)
+            html = element.get_attribute("outerHTML")
+            soup = BeautifulSoup(html, 'html.parser')
 
-        # DOM parse
-        html = self.driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
+            # 1+1 영화 리스트
+            for list in soup.find_all('li'):
 
-        # 1+1 영화 리스트
-        for list in soup.find('ul', class_='product_slist p10').find_all('li'):
+                id = list.get('id')
 
-            id = list.get('id')
+                if id and id not in self.log:
+                    title = list.find('dt', class_='product_tit').getText()
+                    date = list.find('dd', class_='date').getText()
+                    price = list.find('span', class_='price').getText()
+                    img = list.find('img')['src']
+                    text = title + '\n' + date + '\n' + price + '\n' + img
+                    result = telegrambot.send_message(text)
+                    self.log.append(id)
+                    self.count = self.count + 1
 
-            if id and id not in self.log:
-                title = list.find('dt', class_='product_tit').getText()
-                date = list.find('dd', class_='date').getText()
-                price = list.find('span', class_='price').getText()
-                img = list.find('img')['src']
-                text = title + '\n' + date + '\n' + price + '\n' + img
-                result = telegrambot.send_message(text)
-                print(result)
-                self.log.append(id)
-                self.count = self.count + 1
+            # 결과
+            if self.count > 0:
+                filewriter.save_log_file(self.FILE_NAME, self.log)
+                print('%s: 새로운 핫딜 %d개가 등록 되었습니다.'%(self.FILE_NAME,self.count))
+            else:
+                print('%s: 새로운 핫딜이 없습니다.'%self.FILE_NAME)
 
-        # 결과
-        if self.count > 0:
-            filewriter.save_log_file(self.FILE_NAME, self.log)
-            print('%s: 새로운 핫딜 %d개가 등록 되었습니다.'%(self.FILE_NAME,self.count))
-        else:
-            print('%s: 새로운 핫딜이 없습니다.'%self.FILE_NAME)
+        except Exception as errorMessage:
+            print(errorMessage)
+            telegrambot.send_message(errorMessage)
 
 if __name__ == "__main__":
 
