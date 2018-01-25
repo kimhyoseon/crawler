7a1b8ef43fc6d7b4557ff460386ace75d2abb4dc
kimhyoseon <khs7515@naver>
2018-01-25 18:14:22 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -147,8 +147,9 @@ class Crawler:
                 return True
             else:
                 return False
-        except Exception:
-            return False
+        except Exception as e:
+        self.log.logger.error(e, exc_info=True)
+        return False
 
     # 프록시 서버 IP 1개 획득
     def get_proxy_server_ip_list(self):
