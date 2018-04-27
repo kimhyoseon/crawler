0b88eac78409e05717d66d0ae7b16ebe229c559f
kimhyoseon <khs7515@naver>
2018-04-27 12:10:21 +0900

    commit

--- log.py
+++ log.py
@@ -19,7 +19,7 @@ logging_config = dict(
         'file_handler': {'class': 'logging.handlers.RotatingFileHandler',
               'formatter': 'formatter',
               'level': logging.INFO,
-              'filename': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log/print' + datetime.now().strftime('print_%Y_%m_%d.log')),
+              'filename': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log/print/' + datetime.now().strftime('print_%Y_%m_%d.log')),
               'encoding': 'utf-8',
               'maxBytes': 1048576}
               #'backupCount': 0}
