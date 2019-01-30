d41f7031a7d0da067d215264026dae67c5273425
kimhyoseon <khs7515@naver>
2019-01-30 17:10:20 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -100,6 +100,8 @@ class Crawler:
                 options = webdriver.ChromeOptions()
                 options.add_argument("--headless");
                 options.add_argument("--no-sandbox");
+                options.add_argument("--disable-gpu");
+                options.add_argument("--window-size=1920,1080");
                 if is_proxy is True:
                     options.add_argument('--proxy-server=' + proxy_ip)
                 self.driver = webdriver.Chrome(executable_path=self.PATH_CHROME_DRIVER, chrome_options=options)
