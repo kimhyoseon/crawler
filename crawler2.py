36efe7b2e06bdf507fb679578b89af2fc36928cd
kimhyoseon <khs7515@naver.com>
2019-06-13 11:46:24 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -103,8 +103,8 @@ class Crawler:
 
                 # options.add_argument("--headless");
                 # options.add_argument("--no-sandbox");
-                # options.add_argument("--disable-gpu");
-                # options.add_argument("--window-size=1920,1080");
+                options.add_argument("--disable-gpu");
+                options.add_argument("--window-size=1920,1080");
 
                 options.add_argument("user-data-dir=" + self.PATH_USER_DATA)
 
