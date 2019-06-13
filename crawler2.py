8543426ae11e5cb92aa952b717f9cbe9ebbf6398
kimhyoseon <khs7515@naver.com>
2019-06-13 11:45:54 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -101,10 +101,10 @@ class Crawler:
             if is_chrome is True:
                 options = webdriver.ChromeOptions()
 
-                options.add_argument("--headless");
-                options.add_argument("--no-sandbox");
-                options.add_argument("--disable-gpu");
-                options.add_argument("--window-size=1920,1080");
+                # options.add_argument("--headless");
+                # options.add_argument("--no-sandbox");
+                # options.add_argument("--disable-gpu");
+                # options.add_argument("--window-size=1920,1080");
 
                 options.add_argument("user-data-dir=" + self.PATH_USER_DATA)
 
