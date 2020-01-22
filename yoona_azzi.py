0b77dc35b4f6050bb4504026ec20550e627d6bd0
kimhyoseon <khs7515@naver.com>
2020-01-22 14:32:32 +0900

    commit

--- yoona_azzi.py
+++ yoona_azzi.py
@@ -10,6 +10,7 @@ import telegrambot
 from time import sleep
 from proxy import Proxy
 from pytz import timezone
+from random import uniform
 from datetime import datetime, timedelta
 from crawler2 import Crawler
 
@@ -20,85 +21,60 @@ class YoonaAzzi(Crawler):
         # 서울
         # 경기
         '과천(래미안에코팰리스)' : '22779',
-        # '분당(정자동파크뷰)': '3621',
-        # '광명(철산래미안자이)': '25902',
-        # '안양(향촌롯데)': '1480',
-        # '산본(래미안하이어스)': '101283',
-        # '안산(안산메트로타운푸르지오힐스테이트)': '111264',
-        # '구리(신명)': '3577',
-        # '의왕(인덕원푸르지오엘센트로)': '114329',
-        # '부천(중동리첸시아)': '27435',
-        # '수원(자연앤힐스테이트)': '101273',
-        # '용인(성복역롯데캐슬골드타운)': '111555',
-        # '고양(삼송2차아이파크)': '106995',
-        # '화성(시범우남퍼스트빌)': '105405',
-        # '김포(한강신도시반도유보라2차)': '103407',
-        # # 광역시
-        # '인천연수구(송도센트럴파크푸르지오)': '105037',
-        # '인천서구(청라푸르지오)': '100594',
-        # '울산남구(신정푸르지오)': '102946',
-        # '울산북구(울산송정반도유보라아이비파크)': '115848',
-        # '대전서구(크로바)': '5986',
-        # '대전유성구(도룡SK뷰)': '114293',
-        # '부산해운대구(해운대자이2차)': '110398',
-        # '대구수성구(범어SK뷰)': '25632',
-        # '광주광산구(해솔마을현진에버빌1단지)': '101762',
-        # '세종(새뜸6단지힐스테이트메이저시티)': '109173',
-        # # 강원도
-        # '강릉(홍제동우미린)': '108396',
-        # '원주(무실우미린)': '107637',
-        # # 충청북도
-        # '청주(두산위브지웰시티)': '104637',
-        # # 충청남도
-        # '천안(천안불당지웰더샵)': '108766',
-        # # 전라북도
-        # '전주(서부신시가지아이파크)': '23720',
-        # # 전라남도
-        # '목포(한라비발디)': '24772',
-        # '여수(웅천지)': '101892',
-        # '순천(중흥S-클래스메가타운6단지)': '105572',
-        # # 경상북도
-        # '포항(포항자이)': '113172',
-        # '구미(옥계현진에버빌)': '25490',
-        # # 경상남도
-        # '창원(용지아이파크)': '109465',
-        # '김해(부원역푸르지오)': '104220',
-        # '양산(양산대방노블랜드7차메가시티)': '109396',
+        '분당(정자동파크뷰)': '3621',
+        '광명(철산래미안자이)': '25902',
+        '안양(향촌롯데)': '1480',
+        '산본(래미안하이어스)': '101283',
+        '안산(안산메트로타운푸르지오힐스테이트)': '111264',
+        '구리(신명)': '3577',
+        '의왕(인덕원푸르지오엘센트로)': '114329',
+        '부천(중동리첸시아)': '27435',
+        '수원(자연앤힐스테이트)': '101273',
+        '용인(성복역롯데캐슬골드타운)': '111555',
+        '고양(삼송2차아이파크)': '106995',
+        '화성(시범우남퍼스트빌)': '105405',
+        '김포(한강신도시반도유보라2차)': '103407',
+        # 광역시
+        '인천연수구(송도센트럴파크푸르지오)': '105037',
+        '인천서구(청라푸르지오)': '100594',
+        '울산남구(신정푸르지오)': '102946',
+        '울산북구(울산송정반도유보라아이비파크)': '115848',
+        '대전서구(크로바)': '5986',
+        '대전유성구(도룡SK뷰)': '114293',
+        '부산해운대구(해운대자이2차)': '110398',
+        '대구수성구(범어SK뷰)': '25632',
+        '광주광산구(해솔마을현진에버빌1단지)': '101762',
+        '세종(새뜸6단지힐스테이트메이저시티)': '109173',
+        # 강원도
+        '강릉(홍제동우미린)': '108396',
+        '원주(무실우미린)': '107637',
+        # 충청북도
+        '청주(두산위브지웰시티)': '104637',
+        # 충청남도
+        '천안(천안불당지웰더샵)': '108766',
+        # 전라북도
+        '전주(서부신시가지아이파크)': '23720',
+        # 전라남도
+        '목포(한라비발디)': '24772',
+        '여수(웅천지)': '101892',
+        '순천(중흥S-클래스메가타운6단지)': '105572',
+        # 경상북도
+        '포항(포항자이)': '113172',
+        '구미(옥계현진에버빌)': '25490',
+        # 경상남도
+        '창원(용지아이파크)': '109465',
+        '김해(부원역푸르지오)': '104220',
+        '양산(양산대방노블랜드7차메가시티)': '109396',
     }
 
     URL = 'https://new.land.naver.com/api/articles/complex/%s?realEstateType=APT:ABYG:JGC&tradeType=&tag=::::::::&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=true&minMaintenanceCost&maxMaintenanceCost&priceType=RETAIL&directions=&complexNo=22779&buildingNos=&areaNos=&type=list&order=rank&page=%s'
     REQUEST_HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    # PROXY_HOST = '125.138.129.101:4145'
-    # PROXIES = {
-    #     "https": 'https://%s' % PROXY_HOST,
-    #     "http": 'http://%s' % PROXY_HOST,
-    # }
-
     def start(self):
         try:
             # 프록시
-            proxy = Proxy()
-
-            while 1:
-                # getting the current proxy
-                cur_proxy = proxy.proxy
-
-                print(cur_proxy)
-
-                # testing the current proxy
-                res = proxy.test_proxy(cur_proxy)
-
-                if res == 1:
-                    print("success!")
-                    break
-                else:
-                    print("failure!")
-                    proxy.cycle()
-
-            exit()
-
-
+            self.proxy = Proxy("KR")
+            self.cur_proxy = self.proxy.proxy
             self.notice = ''
             self.log = filewriter.get_log_file(self.name)
             self.data = filewriter.get_log_file('yoonaazzi_data', is_json=True)
@@ -148,7 +124,7 @@ class YoonaAzzi(Crawler):
                     # 로그 저장
                     self.setLog(apt=apt)
 
-                    sleep(1)
+                    sleep(round(uniform(0.5,1.5), 1))
                 except:
                     pass
 
@@ -190,16 +166,19 @@ class YoonaAzzi(Crawler):
     # 가격 수집
     def collect_price(self, id=''):
         # url 생성 (아파트번호, 페이지)
-        url = self.URL % (id, self.page)
-        res = requests.get(url, headers=self.REQUEST_HEADER, proxies=self.PROXIES)
-
-        print(url)
-        print(res.content)
-        exit()
+        try:
+            url = self.URL % (id, self.page)
+            res = requests.get(url, headers=self.REQUEST_HEADER, proxies=self.cur_proxy)
+            data = res.json()
+        except:
+            # 데이터를 가져오지 못했다면 프록시 다시 생성
+            self.proxy.cycle()
+            self.cur_proxy = self.proxy.proxy
+            log.logger.info('[%s] try proxy...' % (self.cur_proxy['http']))
+            sleep(round(uniform(0.5,1.5), 1))
+            self.collect_price()
 
         try:
-            data = res.json()
-
             if not data:
                 return False
 
