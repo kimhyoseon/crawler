c373c46ad6320ed4544cce2bc10bd87e61f1b3a1
kimhyoseon <khs7515@naver.com>
2020-04-29 10:07:13 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -205,12 +205,12 @@ class SmartstoreOrderJshk(Crawler):
 
             print(self.driver.current_url)
 
-            if self.connect(site_url='https://help.naver.com/', is_proxy=False, default_driver='selenium', is_chrome=True) is False:
+            if self.connect(site_url='https://nid.naver.com/nidlogin.login', is_proxy=False, default_driver='selenium', is_chrome=True) is False:
                 raise Exception('site connect fail')
 
             print(self.driver.current_url)
 
-            if self.connect(site_url='https://nid.naver.com/nidlogin.login', is_proxy=False, default_driver='selenium', is_chrome=True) is False:
+            if self.connect(site_url='https://help.naver.com/', is_proxy=False, default_driver='selenium', is_chrome=True) is False:
                 raise Exception('site connect fail')
 
             print(self.driver.current_url)
