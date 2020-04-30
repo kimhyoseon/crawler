17b317d093df7399430566aa89503d2caa032437
kimhyoseon <khs7515@naver.com>
2020-04-30 11:29:56 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -132,8 +132,8 @@ class SmartstoreOrderJshk(Crawler):
 
             return True
         except Exception as e:
-            # self.driver.save_screenshot('smartstore_screenshot.png')
             log.logger.error(e, exc_info=True)
+            self.driver.save_screenshot('smartstore_screenshot.png')
             self.destroy()
             exit()
 
