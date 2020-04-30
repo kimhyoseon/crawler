ef548b7be5fe34499f08d468818891a07967f9a9
kimhyoseon <khs7515@naver.com>
2020-04-30 12:09:37 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -96,14 +96,16 @@ class SmartstoreOrderJshk(Crawler):
 
 
             # -- 발송대기 주문 페이지로 이동 --
-
+            print('1')
             if self.selenium_click_by_xpath(xpath='//*[@id="__app_root__"]/div/div[2]/div[1]/div/div[2]/ul/li[4]/div/a[2]') is False:
                 raise Exception('selenium_click_by_xpath fail. orddel.wait')
-
+            print('2')
             sleep(5)
+            print('3')
 
             # 주문 데이터 가져오기 iframe으로 변경
             list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
+            print('4')
 
             for i, li in enumerate(list):
                 try:
