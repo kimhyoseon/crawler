8115d06cbfa72b459a671e5b99df11a4783122bd
kimhyoseon <khs7515@naver.com>
2020-04-30 13:08:29 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -95,7 +95,7 @@ class SmartstoreOrderJshk(Crawler):
                     exit()
 
             # -- 발송대기 주문 페이지로 이동 --
-            if self.selenium_click_by_xpath(xpath='//*[@id="__app_root__"]/div/div[2]/div[1]/div/div[2]/ul/li[4]/div/a[2]') is False:
+            if self.selenium_click_by_xpath(xpath='//*[@id="__app_root__"]/div/div[2]/div[2]/div[1]/table/tbody/tr[1]/td/div/ul/li[3]/a') is False:
                 raise Exception('selenium_click_by_xpath fail. orddel.wait')
 
             sleep(5)
@@ -105,7 +105,6 @@ class SmartstoreOrderJshk(Crawler):
             # exit()
 
             # 주문 데이터 가져오기 iframe으로 변경
-            self.driver.switch_to.frame(frame_reference=self.driver.find_element_by_xpath('//iframe[@id="__naverpay"]'))
             list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
 
             for i, li in enumerate(list):
