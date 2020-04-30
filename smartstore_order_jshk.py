2e34858ebf52b5f7fd091ae5236ab7cbb00a3f99
kimhyoseon <khs7515@naver.com>
2020-04-30 11:34:48 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -99,6 +99,7 @@ class SmartstoreOrderJshk(Crawler):
                 raise Exception('selenium_click_by_xpath fail. orddel.wait')
 
             sleep(5)
+            self.driver.save_screenshot('smartstore_screenshot.png')
 
             # 주문 데이터 가져오기 iframe으로 변경
             list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
@@ -133,7 +134,6 @@ class SmartstoreOrderJshk(Crawler):
             return True
         except Exception as e:
             log.logger.error(e, exc_info=True)
-            self.driver.save_screenshot('smartstore_screenshot.png')
             self.destroy()
             exit()
 
