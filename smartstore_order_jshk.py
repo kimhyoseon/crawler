e5ecc56a062da6ea7c3f0d9382954ddf0d573bf0
kimhyoseon <khs7515@naver.com>
2020-04-30 12:51:41 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -105,6 +105,7 @@ class SmartstoreOrderJshk(Crawler):
             # exit()
 
             # 주문 데이터 가져오기 iframe으로 변경
+            self.driver.switch_to.frame(frame_reference=self.driver.find_element_by_xpath('//iframe[@id="__naverpay"]'))
             list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
 
             for i, li in enumerate(list):
