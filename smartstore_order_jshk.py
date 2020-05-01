6e590a928f18c4bd9a0afd7aaac53d5fa0ab872a
kimhyoseon <khs7515@naver.com>
2020-05-01 15:33:20 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -62,38 +62,38 @@ class SmartstoreOrderJshk(Crawler):
 
             # 주문 데이터 가져오기 iframe으로 변경
             self.driver.switch_to.frame(frame_reference=self.driver.find_element_by_xpath('//iframe[@id="__naverpay"]'))
-            list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
-
-            order_list = {}
-
-            for i, li in enumerate(list):
-                try:
-                    if li:
-                        soup_order_info = BeautifulSoup(li.get_attribute('innerHTML'), 'html.parser')
-                        tds = soup_order_info.find_all('td')
-
-                        if tds:
-                            item_option = tds[16].getText().strip()
-                            item_amount = tds[18].getText().strip()
-                            item_amount = int(item_amount)
-                            destination = tds[40].getText()
-
-                            print(item_option)
-
-                            if item_option not in order_list:
-                                order_list[item_option] = item_amount
-                            else:
-                                order_list[item_option] = order_list[item_option] + item_amount
-
-                            # 제주도인 경우 알림
-                            if destination in '제주특별자치도':
-                                telegrambot.send_message('제주도 주문건을 확인해주세요!.', 'jshk')
-
-
-                except Exception as e:
-                    log.logger.error(e, exc_info=True)
-                    self.destroy()
-                    exit()
+            # list = self.driver.find_element_by_xpath('//*[@id="__app_root__"]/div/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[1]/table').find_elements_by_xpath('.//tbody/tr')
+            #
+            # order_list = {}
+            #
+            # for i, li in enumerate(list):
+            #     try:
+            #         if li:
+            #             soup_order_info = BeautifulSoup(li.get_attribute('innerHTML'), 'html.parser')
+            #             tds = soup_order_info.find_all('td')
+            #
+            #             if tds:
+            #                 item_option = tds[16].getText().strip()
+            #                 item_amount = tds[18].getText().strip()
+            #                 item_amount = int(item_amount)
+            #                 destination = tds[40].getText()
+            #
+            #                 print(item_option)
+            #
+            #                 if item_option not in order_list:
+            #                     order_list[item_option] = item_amount
+            #                 else:
+            #                     order_list[item_option] = order_list[item_option] + item_amount
+            #
+            #                 # 제주도인 경우 알림
+            #                 if destination in '제주특별자치도':
+            #                     telegrambot.send_message('제주도 주문건을 확인해주세요!.', 'jshk')
+            #
+            #
+            #     except Exception as e:
+            #         log.logger.error(e, exc_info=True)
+            #         self.destroy()
+            #         exit()
 
             # -- 발송대기 주문 페이지로 이동 --
             if self.selenium_click_by_xpath(xpath='//*[@id="__app_root__"]/div/div[2]/div[1]/div/div[2]/ul/li[4]/div/a[1]') is False:
