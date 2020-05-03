7440b9e6504bc7132e670eb47c7b24f18a4f7e2b
kimhyoseon <khs7515@naver.com>
2020-05-03 16:05:11 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -108,7 +108,7 @@ class Crawler:
                 options.add_argument("--no-sandbox");
                 options.add_argument('--disable-dev-shm-usage')
                 # options.add_argument("--disable-gpu");
-                options.add_argument("--window-size=1920,1920");
+                # options.add_argument("--window-size=1920,1920");
 
                 if is_proxy is True:
                     options.add_argument('--proxy-server=' + proxy_ip)
