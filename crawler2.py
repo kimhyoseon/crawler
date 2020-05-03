be3a8a6244fb14c20a3a25dec4b2938d430012ae
kimhyoseon <khs7515@naver.com>
2020-05-03 16:03:03 +0900

    commit

--- crawler2.py
+++ crawler2.py
@@ -105,8 +105,8 @@ class Crawler:
 
                 options.add_argument("--user-data-dir=" + self.PATH_USER_DATA)
                 options.add_argument("--headless");
-                options.add_argument("--no-sandbox");
-                options.add_argument("--disable-gpu");
+                # options.add_argument("--no-sandbox");
+                # options.add_argument("--disable-gpu");
                 options.add_argument("--window-size=1920,1920");
 
                 if is_proxy is True:
