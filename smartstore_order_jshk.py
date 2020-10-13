29d49998910eeb571f9ad2c79a96a4f89ba5b8c5
kimhyoseon <khs7515@naver.com>
2020-10-13 13:14:49 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -143,8 +143,8 @@ class SmartstoreOrderJshk(Crawler):
                 # self.remove_layer()
 
                 log.logger.info('----')
-                self.destroy()
-                exit()
+                # self.destroy()
+                # exit()
 
                 # 주문 데이터 가져오기 iframe으로 변경
                 self.driver.switch_to.frame(frame_reference=self.driver.find_element_by_xpath('//iframe[@id="__naverpay"]'))
