47887119f91a057bf392aa0ead2db7893c199bfa
kimhyoseon <khs7515@naver.com>
2020-11-04 07:42:01 +0900

    commit

--- encar.py
+++ encar.py
@@ -13,8 +13,14 @@ class Encar(Crawler):
         try:
             self.log = filewriter.get_log_file(self.name)
 
-            # 주행거리 14만 이하, 주행보조, 엔카진단
+            # 제네시스DH 주행거리 14만 이하, 주행보조, 엔카진단
             if self.connect(site_url='http://www.encar.com/dc/dc_carsearchlist.do?carType=kor&searchType=model&wtClick_kor=003&TG.R=A#!%7B%22action%22%3A%22(And.Hidden.N._.(C.CarType.Y._.(C.Manufacturer.%ED%98%84%EB%8C%80._.(C.ModelGroup.%EC%A0%9C%EB%84%A4%EC%8B%9C%EC%8A%A4._.Model.%EC%A0%9C%EB%84%A4%EC%8B%9C%EC%8A%A4%20DH.)))_.Trust.Warranty._.Options.%EC%B0%A8%EC%84%A0%EC%9D%B4%ED%83%88%20%EA%B2%BD%EB%B3%B4%20%EC%8B%9C%EC%8A%A4%ED%85%9C(LDWS_)._.Condition.Inspection._.Condition.Record._.Mileage.range(..140000).)%22%2C%22toggle%22%3A%7B%7D%2C%22layer%22%3A%22%22%2C%22sort%22%3A%22PriceAsc%22%2C%22page%22%3A1%2C%22limit%22%3A20%7D', is_proxy=False, default_driver='selenium', is_chrome=False) is False:
+                raise Exception('site connect fail')
+
+            self.scan_page()
+
+            # 전기차 주행보조
+            if self.connect(site_url='http://www.encar.com/ev/ev_carsearchlist.do?carType=ev&searchType=model&TG.R=D#!%7B%22action%22%3A%22(And.Hidden.N._.CarType.A._.Options.%EC%B0%A8%EC%84%A0%EC%9D%B4%ED%83%88%20%EA%B2%BD%EB%B3%B4%20%EC%8B%9C%EC%8A%A4%ED%85%9C(LDWS_)._.Options.%ED%81%AC%EB%A3%A8%EC%A6%88%20%EC%BB%A8%ED%8A%B8%EB%A1%A4(%EC%96%B4%EB%8C%91%ED%8B%B0%EB%B8%8C_)._.(C.GreenType.Y._.EvType.%EC%A0%84%EA%B8%B0%EC%B0%A8.))%22%2C%22toggle%22%3A%7B%7D%2C%22layer%22%3A%22%22%2C%22sort%22%3A%22PriceAsc%22%2C%22page%22%3A1%2C%22limit%22%3A20%7D', is_proxy=False, default_driver='selenium', is_chrome=False) is False:
                 raise Exception('site connect fail')
 
             self.scan_page()
