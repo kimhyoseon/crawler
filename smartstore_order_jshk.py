b9e5d620448e7d4c026786893c88d82a5236607a
kimhyoseon <khs7515@naver.com>
2020-12-31 15:11:44 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -350,6 +350,9 @@ class SmartstoreOrderJshk(Crawler):
 
             sleep(10)
 
+            html = self.driver.page_source
+            log.logger.info(html)
+
             if self.selenium_extract_by_xpath(tag={'tag': 'input', 'attr': 'id', 'name': 'id'}) is False:
                 raise Exception('selenium_extract_by_xpath fail.')
 
