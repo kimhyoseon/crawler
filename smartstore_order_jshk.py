9b14dbe9327310d6a51f1344b779dc964b274cf2
kimhyoseon <khs7515@naver.com>
2020-12-31 14:05:35 +0900

    commit

--- smartstore_order_jshk.py
+++ smartstore_order_jshk.py
@@ -369,26 +369,32 @@ class SmartstoreOrderJshk(Crawler):
             if account_data:
                 # self.driver.save_screenshot('smartstore_screenshot.png')
 
-                sleep(10)
+                sleep(5)
 
                 # 로그인 페이지로 이동
                 if self.selenium_click_by_xpath(tag={'tag': 'a', 'attr': 'data-nclicks-code', 'name': 'main.sellerlogin'}) is False:
                     raise Exception('selenium_click_by_xpath fail. submit')
 
-                sleep(10)
+                log.logger.info('step 1')
+
+                sleep(5)
 
                 if self.selenium_click_by_xpath(tag={'tag': 'a', 'attr': 'data-nclicks-code', 'name': 'login.nidlogin'}) is False:
                     raise Exception('selenium_click_by_xpath fail. submit')
 
+                log.logger.info('step 2')
+
                 # if self.connect(site_url='http://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fsell.smartstore.naver.com%2F%23%2FnaverLoginCallback%3Furl%3Dhttps%253A%252F%252Fsell.smartstore.naver.com%252F%2523', is_proxy=False, default_driver='selenium', is_chrome=True) is False:
                 #     raise Exception('login page connect fail')
 
-                sleep(60)
+                sleep(30)
 
                 # self.driver.save_screenshot('smartstore_login_screenshot.png')
 
                 if self.selenium_extract_by_xpath(tag={'tag': 'input', 'attr': 'name', 'name': 'id'}) is False:
                     raise Exception('selenium_extract_by_xpath fail.')
+
+                log.logger.info('step 3')
 
                 # 아이디 입력
                 if self.selenium_input_text_by_xpath(text=account_data[0], tag={'tag': 'input', 'attr': 'name', 'name': 'id'}) is False:
