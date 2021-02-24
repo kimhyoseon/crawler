1b6975a2558210ef9fcfa13ba60925fd5f69081c
kimhyoseon <khs7515@naver.com>
2021-02-24 15:29:51 +0900

    commit

--- smartstore_stock_telegram.py
+++ smartstore_stock_telegram.py
@@ -39,7 +39,7 @@ class SmartstoreStockTelegram():
                     if row[7] and row[7] < 15:
                         # print(row)
                         if row[3]:
-                            add = notice + '%s[%s] %d일\n' % (row[2], row[3], int(row[7]))
+                            add = '%s[%s] %d일\n' % (row[2], row[3], int(row[7]))
                             notice = notice + add
                         else:
                             add = '%s %d일\n' % (row[2], int(row[7]))
